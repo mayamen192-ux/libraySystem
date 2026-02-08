5376cbc05ee98193c481f4b0e052a73199946297
Mayamin.sh <Mayamin.sh@MEMLT0040>
2026-02-08 15:12:01 +0400

    edit sqlcode for library

--- SQLLibrary.sql
+++ SQLLibrary.sql
@@ -10,6 +10,7 @@ primary key(LibraryId)
 );
 
 
+
 alter table library
 drop column year
 
@@ -34,17 +35,29 @@ primary key(ID,Location),
 LID int foreign key references Library(LibraryId)
 );
 
-create table book(
-bookID int NOT NULL,
+ALTER TABLE location
+ADD CONSTRAINT FK_Child_Parent
+FOREIGN KEY (LID)
+REFERENCES library1(LibraryId)
+ON DELETE CASCADE
+ON UPDATE CASCADE;
+
+create table bookk1(
+bookID int identity(1,1)  NOT NULL,
 ISBN varchar(10) NOT NULL,
 title varchar(255),
 price DECIMAL(8,2),
-BLID int foreign key references Library(LibraryId),
+BLID int foreign key references library1(LibraryId),
 primary key(bookID)
 );
 
-create table member(
-memberID int NOT NULL,
+alter table bookk1
+add  LocationShelf varchar(50)
+
+alter table bookk1
+add IsAvailable varchar(100)
+create table member1(
+memberID int  identity(1,1) NOT NULL,
 Fname varchar(100),
 Lname varchar(100),
 phoneNumber VARCHAR(15),
@@ -53,18 +66,25 @@ membershipStartDate date
 primary key(memberID)
 );
 
-alter table Library
-add booksId int foreign key references Library(LibraryId)
+alter table library1
+add booksId int foreign key references book(bookID)
+ON DELETE CASCADE
+ON UPDATE CASCADE;
 
-select * from Library
+
+
+
+
+drop column booksId
+select * from library1
 
 alter table book
 add membersID int foreign key references member(memberID)
 
 select * from book
 
-create table staff(
-staffID int NOT NULL,
+create table staff1(
+staffID int  identity(1,1) NOT NULL,
 Fname varchar(100),
 Lname varchar(100),
 position varchar(50),
@@ -74,36 +94,308 @@ primary key(staffID)
 
 drop table staff 
 
-alter table Library
+alter table library1
 add staffsID int foreign key references staff(staffID)
+ON DELETE CASCADE
+ON UPDATE CASCADE;
+
 
 select * from staff
 
-create table review(
+create table review1(
+reviewID int identity(1,1) NOT NULL,
 rating TINYINT NOT NULL,
 reviewDate date,
 comments VARCHAR(1000),
 memberssID int foreign key references member(memberID),
-primary key(rating)
+primary key(reviewID)
 );
 select * from review
 drop table review
 
-create table loan(
+create table loan1(
+LoanID int identity(1,1) NOT NULL,
 loanDate date Not NULL,
-dueDate date,
-status varchar(255),
+dueDate date NOT NULL ,
+status varchar(255) NOT NULL ,
 returnDate date,
-primary key(loanDate)
+primary key(LoanID)
 );
-select * from loan
 
-create table payment(
-payDate date NOT NULL,
-amount DECIMAL(10,2),
+select * from loan1
+
+create table payment1(
+PayID int identity(1,1) NOT NULL ,
+payDate date  NOT NULL,
+amount DECIMAL(10,2) NOT NULL,
 method varchar(100),
 finePayment DECIMAL(10,2),
-loansDate date foreign key references loan(loanDate)
+primary key(payID)
 );
-select * from payment
+alter table payment1
+add loanID int foreign key references loan1(LoanID)
+ON DELETE CASCADE
+ON UPDATE CASCADE;
+
+
+select * from payment1
+
+alter table loan
+add paysDate date foreign key references payment(payDate)
+
+create table link(
+IDBooks int,
+IDMemerber int,
+LDate date,
+primary key(IDBooks,IDMemerber,LDate),
+IdBook int foreign key references book(bookID),
+IdMember int foreign key references member(memberID),
+Ldates date foreign key references loan(loanDate)
+);
+
+
+create table  link2(
+IDBook2 int ,
+IDMember2 int ,
+rating2 TINYINT,
+primary key(IDBook2,IDMember2,rating2),
+IDbooks int foreign key references book(bookID),
+IDMembers int foreign key references member(memberID),
+ratings TINYINT foreign key references review(rating)
+);
+
+
+create table library1(
+LibraryId int identity(1,1)NOT NULL,
+LibraryName varchar(100) UNIQUE NOT NULL ,
+contactNumber int NOT NULL ,
+Lyear int ,
+primary key(LibraryId)
+
+);
+select * from book
+--------------------------------
+
+ALTER TABLE library
+ADD LibraryId INT IDENTITY(1,1) PRIMARY KEY;
+
+alter table Library
+add UNIQUE (LibraryName)
+
+
+alter table book
+add UNIQUE (ISBN)
+
+alter table member
+add UNIQUE(email)
+
+
+ALTER TABLE Library
+DROP CONSTRAINT UQ__Library__549440CC94D8B5F6;
+
+ALTER TABLE Library
+ALTER COLUMN LibraryName VARCHAR(100) NOT NULL;
+
+ALTER TABLE Library
+ADD CONSTRAINT UQ_Library_LibraryName UNIQUE (LibraryName);
+
+
+
+
+--SELECT LibraryName
+--FROM library
+--WHERE LibraryId IS NOT NULL;
+
+
+
+alter table location
+ALTER COLUMN Location varchar(100) NOT NULL
+
+alter table library
+ALTER COLUMN contactNumber int NOT NULL
+
+alter table bookk1
+ALTER COLUMN title varchar(255) NOT NULL
+
+alter table bookk1
+ALTER COLUMN ISBN varchar(10) NOT NULL
+
+alter table bookk1
+ALTER COLUMN title varchar(255) NOT NULL
+
+alter table bookk1
+ALTER COLUMN ISBN varchar(10) NOT NULL
+
+alter table bookk1
+add Genre varchar(100) NOT NULL
+
+alter table bookk1
+add  ShelfLocation varchar(255) NOT NULL
+
+--ALTER TABLE member
+--alter column  email VARCHAR(254) NOT NULL
+
+
+--ALTER TABLE member
+--DROP CONSTRAINT UQ__member__549440CC94D8B5F6;
+
+--SELECT name, type_desc
+--FROM sys.indexes
+--WHERE object_id = OBJECT_ID('member');
+--DROP INDEX UQ__member__549440CC94D8B5F6 ON member;
+--SELECT  
+--    s.name  AS SchemaName,
+--    t.name  AS TableName,
+--    i.name  AS IndexName
+--FROM sys.indexes i
+--JOIN sys.tables t ON i.object_id = t.object_id
+--JOIN sys.schemas s ON t.schema_id = s.schema_id
+--WHERE i.name = 'UQ__member__549440CC94D8B5F6';
+
+--DROP INDEX UQ__member__549440CC94D8B5F6 
+--ON dbo.member;
+
+
+
+
+ALTER TABLE member
+ALTER COLUMN email VARCHAR(254) NOT NULL;
+
+ALTER TABLE member
+ADD CONSTRAINT UQ_member_email UNIQUE (email);
+
+alter table member
+add  ShelfLocation varchar(255) NOT NULL 
+
+alter table loan
+alter column   dueDate date NOT NULL 
+
+alter table loan
+alter column status varchar(255) NOT NULL 
+
+
+alter table payment1
+alter column amount DECIMAL(10,2) NOT NULL 
+
+alter table review1
+alter column reviewDate date NOT NULL 
+-------------------------------------------
+select * from bookk1
+ALTER TABLE bookk1
+ADD CONSTRAINT CK_Book_Genre1
+CHECK (Genre IN ('Fiction', 'Non-fiction', 'Reference', 'Children'));
+
+ALTER TABLE loan1
+ADD CONSTRAINT CK_Loan_Status1
+CHECK (Status IN ('Issued', 'Returned', 'Overdue'));
+
+ALTER TABLE bookk1
+ADD CONSTRAINT CK_Book_Price_Positive1
+CHECK (Price > 0);
+
+ALTER TABLE payment1
+ADD CONSTRAINT CK_Payment_Amount_Positive1
+CHECK (amount > 0);
+
+ALTER TABLE loan1
+ADD CONSTRAINT CK_Loan_ReturnDate1
+CHECK (returnDate >= loanDate);
+
+ALTER TABLE review1
+ADD CONSTRAINT CK_Review_Rating1
+CHECK (rating BETWEEN 1 AND 5);
+
+ALTER TABLE bookk1
+ADD CONSTRAINT DF_Book_IsAvailable1
+DEFAULT 1 FOR IsAvailable;
+
+ALTER TABLE loan1
+ADD CONSTRAINT DF_Loan_Status1
+DEFAULT 'Issued' FOR status;
+
+ALTER TABLE review1
+ADD CONSTRAINT DF_Review_Comments1
+DEFAULT 'No comments' FOR comments;
+select * from review1
+-----------------------
+insert into library1(LibraryName,contactNumber,Lyear,booksId,staffsID)
+values('History',99667754,2001, NULL, NULL),
+('computing',98754308,2009, NULL, NULL),
+('Islamic',93246129,2017, NULL, NULL),
+('computing',90561237,2001, NULL, NULL),
+('business',99291357,2009, NULL, NULL),
+('Islamic',99976543,2005, NULL, NULL),
+('business',90091235,2006, NULL, NULL),
+('computing',93698123,2015, NULL, NULL),
+('History',99807054,2012, NULL, NULL),
+('History',938129542,2018, NULL, NULL);
+
+
+ALTER TABLE bookk1
+DROP column BLID;
+select * from link2
+
+
+ insert into bookk1(title,ISBN,	price,BLID,isAvailable,Genre,ShelfLocation)
+ values('cloud computing',1234,20,NULL,'True','Reference','New York'),
+ ('human resources',1223,20,NULL,'True','Children','Massachusetts'),
+ ('praying',1223,20,NULL,'True','Reference','Boston'),
+ ('netwoking',1981,30,NULL,'Flase','Reference','New York'),
+ ('education with funny',1265,20,NULL,'True','Children','Boston'),
+  ('education with funny',1265,20,NULL,'Flase','Children','Boston'),
+ ('Foundation of historical writing',1232,70,NULL,'True','Non-fiction','Massachusetts'),
+ ('sciences',1230,40,NULL,'True','fiction','Massachusetts');
+
+
+ insert into member(memberID,Fname,Lname,phoneNumber,email,membershipStartDate,ShelfLocation)
+ values(1,'Ali','Mohammed',90085541,'a@gmail.com','2023-09-07','New York'),
+ (2,'Ahmed','Mohammed',99812354,'ah@gmail.com','2023-08-03','Massachusetts'),
+  (3,'Sara','Salam',92132179,'s@gmail.com','2024-08-21','Massachusetts'),
+    (4,'Fatma','Salam',93232189,'f@gmail.com','2024-10-09','New York'),
+	 (5,'Salah','Ali',99932178,'sa@gmail.com','2025-11-09','New York'),
+	  (6,'Heba','Ahmed',98712453,'h@gmail.com','2024-12-20','Massachusetts'),
+	  (7,'maram','Ahmed',98332121,'mr@gmail.com','2023-10-20','Massachusetts');
+
+insert into staff1(Fname,Lname,position,contactNumber)
+values('Ali','Mohammed','Oman',90087541),
+('Sara','Ali','America',91234567),
+('Fares','Ali','UAE',99878687),
+('Eman','Saif','Oman',91234543),
+('Ahmed','Hasem','Bahrain',91214318),
+('Ameera','Ahmed','Oman',912354127),
+('Shaed','Ali','America',912378541);
+
+insert into review1(rating,reviewDate,comments,memberssID)
+values(2,'2023-02-02','add new member',3),
+(4,'2024-12-12','edit member',2),
+(5,'2025-09-12','delete member',7),
+(3,'2025-03-09','edit member',4),
+(1,'2023-09-11','add new member',6),
+(2,'2025-09-12','delete member',7),
+(5,'2025-12-12','delete member',5);
+
+
+insert into loan1(loanDate,dueDate,status,returnDate)
+values('2025-09-06','2025-09-16','Returned','2025-09-26'),
+('2025-07-01','2025-07-11','Issued','2025-07-16'),
+('2025-02-06','2025-02-16','Returned','2025-02-26'),
+('2025-05-15','2025-05-25','Overdue','2025-06-05'),
+('2025-07-06','2025-07-16','Returned','2025-07-26'),
+('2025-01-06','2025-01-16','Issued','2025-01-26');
+
+insert into payment1(payDate,amount,method,finePayment,loanID)
+values('2025-02-23',20,'cash',0,2),
+('2025-09-14',15,'visa',0,3),
+('2025-05-27',30,'no pay',10,1),
+('2025-07-12',20,'visa',0,5);
+
+insert into link(IDBooks,IDMemerber,LDate)
+values(2,1,'2025-07-01'),
+(1,2,'2025-09-06'),
+(3,4,'2025-01-06');
+
+insert into link2(IDBook2,IDMember2,rating2)
+values(2,1,3),
+(1,2,2),
+(3,4,1);
 
